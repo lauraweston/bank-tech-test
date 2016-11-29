60f29ccbcf09a2bf6ca016ba4dfd61d39f94d4f8
Laura Weston <laura.weston@outlook.com>
2016-11-29 16:35:22 +0000

    Inject Credit and Debit class into Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,33 +1,36 @@
+
 class Account
   attr_reader :transactions
 
-  def initialize
+  def initialize(credit, debit)
     @pence = 0
     @transactions = []
+    @credit = credit
+    @debit = debit
   end
 
   def balance
-    "#{convert_pence_to_balance}"
+    "£#{convert_pence_to_balance}"
   end
 
   def deposit(amount)
     deposited_pence = convert_to_pence(amount)
     @pence += deposited_pence
-    @transactions << Credit.new(deposited_pence)
+    @transactions << credit.new(deposited_pence)
   end
 
   def withdraw(amount)
     withdrawn_pence = convert_to_pence(amount)
     if pence >= withdrawn_pence
       @pence -= withdrawn_pence
-      @transactions << Debit.new(withdrawn_pence)
+      @transactions << debit.new(withdrawn_pence)
     else
-      raise "Insufficient funds: available balance £#{balance}"
+      raise "Insufficient funds: available balance #{balance}"
     end
   end
 
   private
-  attr_reader :pence
+  attr_reader :pence, :credit, :debit
 
   def convert_to_pence(amount)
     amount.delete(".").to_i
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,18 @@
 require_relative "../lib/account"
-require_relative "../lib/credit"
-require_relative "../lib/debit"
+# require_relative "../lib/credit"
+# require_relative "../lib/debit"
 
 describe Account do
-  subject(:account) { described_class.new }
+  let(:credit_class) { double :credit_class }
+  let(:debit_class) { double :debit_class }
+  subject(:account) { described_class.new(credit_class, debit_class) }
+
+  before :each do
+    allow(credit_class).to receive(:new)
+  end
 
   it "initializes with a balance of zero" do
-    expect(account.balance).to eq "0.00"
+    expect(account.balance).to eq "£0.00"
   end
 
   describe "statement" do
@@ -17,25 +23,25 @@ describe Account do
 
   describe "depositing" do
     context "initial balance is zero" do
-      let(:credit) { double :credit }
+      let(:credit_instance) { double :credit_instance }
 
       it "changes the balance by the deposited amount" do
         account.deposit("10.50")
 
-        expect(account.balance).to eq "10.50"
+        expect(account.balance).to eq "£10.50"
       end
 
       it "creates a new instance of Credit" do
-        expect(Credit).to receive(:new).with(1050)
+        expect(credit_class).to receive(:new).with(1050)
         account.deposit("10.50")
       end
 
       it "adds the Credit instance to the transactions list" do
-        allow(Credit).to receive(:new).with(1050).and_return(credit)
+        allow(credit_class).to receive(:new).with(1050).and_return(credit_instance)
         account.deposit("10.50")
 
         expect(account.transactions.length).to eq 1
-        expect(account.transactions.last).to eq credit
+        expect(account.transactions.last).to eq credit_instance
       end
     end
 
@@ -44,15 +50,16 @@ describe Account do
         account.deposit("10.50")
         account.deposit("5.75")
 
-        expect(account.balance).to eq "16.25"
+        expect(account.balance).to eq "£16.25"
       end
     end
   end
 
   describe "withdrawing" do
-    let(:debit) { double :debit }
+    let(:debit_instance) { double :debit_instance }
 
     before :each do
+      allow(debit_class).to receive(:new)
       account.deposit("10.00")
     end
 
@@ -60,33 +67,33 @@ describe Account do
       it "reduces the balance by the withdrawn amount" do
         account.withdraw("4.25")
 
-        expect(account.balance).to eq "5.75"
+        expect(account.balance).to eq "£5.75"
       end
 
       it "allows withdrawals of full remaining balance" do
         account.withdraw("10.00")
 
-        expect(account.balance).to eq "0.00"
+        expect(account.balance).to eq "£0.00"
       end
 
       it "creates a new instance of Debit" do
-        expect(Debit).to receive(:new).with(550)
+        expect(debit_class).to receive(:new).with(550)
         account.withdraw("5.50")
       end
 
       it "adds the Debit instance to the transactions list" do
-        allow(Debit).to receive(:new).with(550).and_return(debit)
+        allow(debit_class).to receive(:new).with(550).and_return(debit_instance)
         account.withdraw("5.50")
 
         expect(account.transactions.length).to eq 2
-        expect(account.transactions.last).to eq debit
+        expect(account.transactions.last).to eq debit_instance
       end
     end
 
     context "withdrawal amount is greater than account balance" do
       it "does not allow the amount to be withdrawn" do
         expect { account.withdraw("10.50") }.to raise_error "Insufficient funds: available balance £10.00"
-        expect(account.balance).to eq "10.00"
+        expect(account.balance).to eq "£10.00"
       end
     end
   end
