0080c836691d43ac524537bc0408aa5a76d8e4e8
Laura Weston <laura.weston@outlook.com>
2016-11-29 12:23:37 +0000

    Add #deposit to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,18 @@
 class Account
   def initialize
-    @pounds = 0
     @pence = 0
   end
 
   def balance
-    pennies = pence.to_s.rjust(2, "0")
-    "#{pounds}.#{pennies}"
+    balance = pence.to_s.rjust(3, "0").insert(-3, ".")
+    "#{balance}"
+  end
+
+  def deposit(amount)
+    deposited_pence = amount.delete(".").to_i
+    @pence += deposited_pence
   end
 
   private
-  attr_reader :pounds, :pence
+  attr_reader :pence
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,25 @@
 require_relative "../lib/account"
 
 describe Account do
+  subject(:account) { described_class.new }
+
   it "initializes with a balance of zero" do
-    account = Account.new
     expect(account.balance).to eq "0.00"
+  end
+
+  describe "depositing" do
+    context "initial balance is zero" do
+      it "changes the balance by the deposited amount" do
+        account.deposit("10.50")
+        expect(account.balance).to eq "10.50"
+      end
+    end
+    context "initial balance is greater than zero" do
+      it "changes the balance by the deposited amount" do
+        account.deposit("10.50")
+        account.deposit("5.75")
+        expect(account.balance).to eq "16.25"
+      end
+    end
   end
 end
