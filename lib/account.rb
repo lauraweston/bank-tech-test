dc4f55cf8a23c1f6d19bb1afc3bdbea03531cb9a
Laura Weston <laura.weston@outlook.com>
2016-11-30 22:37:27 +0000

    Refactor Account class, replacing @pence attr with #calculate_balance

--- lib/account.rb
+++ lib/account.rb
@@ -3,7 +3,6 @@ class Account
   attr_reader :transactions
 
   def initialize(credit, debit, print_statement)
-    @pence = 0
     @transactions = []
     @credit = credit
     @debit = debit
@@ -11,22 +10,22 @@ class Account
   end
 
   def balance
-    "£#{convert_pence_to_balance}"
+    "£#{format_pence(calculate_balance)}"
   end
 
   def deposit(amount)
     deposited_pence = convert_to_pence(amount)
     transaction = credit.new(deposited_pence)
-    update_account(transaction)
+    update_transactions(transaction)
   end
 
   def withdraw(amount)
     withdrawn_pence = convert_to_pence(amount)
-    if withdrawn_pence > pence
+    if withdrawn_pence > calculate_balance
       raise "Insufficient funds: available balance #{balance}"
     end
     transaction = debit.new(withdrawn_pence)
-    update_account(transaction)
+    update_transactions(transaction)
   end
 
   def statement
@@ -40,20 +39,15 @@ class Account
     amount.delete(".").to_i
   end
 
-  def convert_pence_to_balance
+  def format_pence(pence)
     pence.to_s.rjust(3, "0").insert(-3, ".")
   end
 
-  def update_balance(amount)
-    @pence += amount
+  def calculate_balance
+    transactions.reduce(0) { |balance, transaction| balance + transaction.amount }
   end
 
   def update_transactions(transaction)
     @transactions << transaction
-  end
-
-  def update_account(transaction)
-    update_balance(transaction.amount)
-    update_transactions(transaction)
   end
 end
